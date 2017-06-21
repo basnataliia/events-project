985cc908cfcdc6c37f0d00409d7c933f7304f589
Natalie Basmanova <natalie.basmanova@futurevaultinc.com>
2017-06-21 15:05:09 -0400

    event item styles updates

--- src/Components/EventItem/EventItem.js
+++ src/Components/EventItem/EventItem.js
@@ -3,11 +3,12 @@ import {PropTypes} from 'prop-types';
 import {Link} from 'react-router';
 import DateFormat from '../DateFormat/DateFormat';
 import {Card, CardHeader, CardMedia, CardTitle} from 'material-ui/Card';
+import './EventItem.css';
 
 const EventItem = ({event}) => {
   let title = '';
-  if(event.title.length > 50) {
-    title = `${event.title.substring(0, 47)} ...`;
+  if(event.title.length > 45) {
+    title = `${event.title.substring(0, 42)} ...`;
   } else {
     title = event.title;
   }
@@ -19,7 +20,8 @@ const EventItem = ({event}) => {
         subtitle={<DateFormat pubDate={event.start_time} />}
       />
       <CardMedia
-        overlay={<CardTitle title={<span style={{fontSize:'14px', lineHeight:'5'}}>Where: {event.venue_name}</span>}/>}
+        overlay={<CardTitle className="card-title" title={<span style={{fontSize:'14px', lineHeight:'5px'}}>
+          Where: {event.venue_name}</span>}/>}
       >
         <img src={event.image.perspectivecrop290by250.url} style={{height: '276px'}}/>
       </CardMedia>
